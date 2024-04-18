e5a245768d963433afb6396aaa4b86d28776f9f8
alok2011 <alokranjantiwaricse@gmail.com>
2024-04-18 16:28:54 +0530

    update

--- src/main/java/infosys/client/Runner.java
+++ src/main/java/infosys/client/Runner.java
@@ -12,7 +12,8 @@ public class Runner
 
 	public static void main(String[] args) 
 	{
-		Student Alok = new Student(2,"Prashant","Male","Delhi");
+		Student Alok = new Student(3,"Ramakant","Male","Delhi");
+		System.out.println("HQL");
 		SessionFactory sf = HibernateConfiguration.getSessionFactory();
 		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
